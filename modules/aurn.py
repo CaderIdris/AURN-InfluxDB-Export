4db62f5d6c60d77e8532fad18afb0171e17f8009
Joppleganger <61825162+Joppleganger@users.noreply.github.com>
2021-06-01 09:50:46 +0100

    Made things look nicer, proper PEP8 now

--- modules/aurn.py
+++ modules/aurn.py
@@ -2,8 +2,8 @@
 to download metadata and measurements for the AURN
 
 The AURN has no official API for Python so metadata and measurements have to
-be obtained by scraping HTML data from the DEFRA website. This module 
-handles all communications with the DEFRA website, obtaining metadata and 
+be obtained by scraping HTML data from the DEFRA website. This module
+handles all communications with the DEFRA website, obtaining metadata and
 measurements
 
     Classes:
@@ -23,8 +23,9 @@ __status__ = "Alpha"
 import requests as req
 from lxml import html  # Needed to scrape AURN website for metadata
 import pandas as pd
-import datetime as dt 
+import datetime as dt
 import urllib  # Needed for pandas error when csv not present
+
 
 def remove_brackets(string_with_brackets):
     """ Removes brackets and the content within them from an input string
@@ -37,7 +38,7 @@ def remove_brackets(string_with_brackets):
         string_with_brackets (str): An input string, presumably with brackets
 
     Variables:
-        bracket_pairs (dict): Keys represent open brackets, items are their 
+        bracket_pairs (dict): Keys represent open brackets, items are their
         matching closers
 
         unbracketed (bool): Is character outside a bracket?
@@ -62,7 +63,7 @@ def remove_brackets(string_with_brackets):
         if character in open_brackets:
             unbracketed = False
             close_bracket = bracket_pairs[character]
-        if unbracketed:        
+        if unbracketed:
             clean_string = f"{clean_string}{character}"
         if not unbracketed and character == close_bracket:
             unbracketed = True
@@ -74,7 +75,6 @@ def remove_brackets(string_with_brackets):
     return clean_string
 
 
-
 class AURNAPI:
     """ Handles communication with the AURN/DEFRA website to get metadata
     and measurements
@@ -84,7 +84,7 @@ class AURNAPI:
 
         metadata (list): Contains dictionaries which house all metadata for
         AURN sites that were active in the specified data range, split in to
-        "tags" for all text info (Site Name etc) and "fields" for location 
+        "tags" for all text info (Site Name etc) and "fields" for location
         info (Latitude etc)
 
         measurement_csv (DataFrame): Most recent measurements downloaded
@@ -102,7 +102,7 @@ class AURNAPI:
         """Initialises class
 
         Keyword arguments:
-            config (dict): Contains info used in class, configured in 
+            config (dict): Contains info used in class, configured in
             config.json
         """
         self.config = config
@@ -115,41 +115,44 @@ class AURNAPI:
         """ Downloads metadata from AURN/DEFRA website
 
         As there's no official Python API for the AURN, this function scrapes
-        the AURN/DEFRA website's HTML source for a) a csv containing all 
-        metadata for all stations in the network and b) The download link for 
+        the AURN/DEFRA website's HTML source for a) a csv containing all
+        metadata for all stations in the network and b) The download link for
         csv files in the network
 
         Keyword arguments:
-            start_year (int): The year the measurement download will start 
+            start_year (int): The year the measurement download will start
             from
 
             end_year (int): The last year the measurement download will cover
 
         Variables:
-            metadata_search_url (str): The url used to search for info on all 
+            metadata_search_url (str): The url used to search for info on all
             stations in the network
 
             metadata_html_page (request): Returned information from html
             request for metadata
 
-            metadata_html_source (html object): lxml searchable metadata_html_page
+            metadata_html_source (html object): lxml searchable
+            metadata_html_page
 
-            metadata_csv_link (str): Link to metadata csv, obtained by 
+            metadata_csv_link (str): Link to metadata csv, obtained by
             searching html with an XPath string
 
             site_start_year (int): The year the site started operating
 
-            site_end_year (int): The year the site ended operating. 
+            site_end_year (int): The year the site ended operating.
             Set to current year + 1 if still in operation (nan in csv)
-            
-            start_year_in_range (bool): Do start_year and end_year fall within site_start_year?
 
-            end_year_in_range (bool): Do start_year and end_year fall within site_end_year?
+            start_year_in_range (bool): Do start_year and end_year fall within
+            site_start_year?
+
+            end_year_in_range (bool): Do start_year and end_year fall within
+            site_end_year?
 
             data_not_available (bool): Does the date range cross the dates
             the site was active?
 
-            not_aurn_site (bool): Is the site an AURN site? Some sites in the 
+            not_aurn_site (bool): Is the site an AURN site? Some sites in the
             csv are duplicates with different names but the same download url,
             one of the duplicates doesn't state it's AURN so this test removes
             them
@@ -162,25 +165,26 @@ class AURNAPI:
             site_info_html_page (request): Returned information for site info
             request
 
-            site_info_html_source (html object): lxml searchable site_info_html_page
+            site_info_html_source (html object): lxml searchable
+            site_info_html_page
 
-            site_info_link_xpath (list): List of all links in tbale on 
+            site_info_link_xpath (list): List of all links in tbale on
             site_info_html_page
 
             download_code (str): Download url for measurement csvs
 
         """
 
-        # Get HTML file with search results of all sites, open or closed, 
+        # Get HTML file with search results of all sites, open or closed,
         # in network
         metadata_search_url = (f"{self.config['AURN Domain']}"
-                f"{self.config['AURN Metadata Search']}")
+                               f"{self.config['AURN Metadata Search']}")
         metadata_html_page = req.get(
-                metadata_search_url, 
+                metadata_search_url,
                 headers={"User-Agent": self.config['User Agent']}
                 )
         metadata_html_source = html.fromstring(metadata_html_page.content)
-                
+
         # Search HTML file for link to csv metadata using xpath
         metadata_csv_link = metadata_html_source.xpath(
                 self.config["XPath to CSV"]
@@ -188,7 +192,7 @@ class AURNAPI:
 
         # Download metadata csv
         metadata_csv = pd.read_table(metadata_csv_link, sep=",")
-        
+
         # Get download code (Usually 3 characters) for all sites
         # e.g Aberdeen(UKA00399) is ABD
         for index, row in metadata_csv.iterrows():
@@ -201,7 +205,7 @@ class AURNAPI:
                         "%Y-%m-%d"
                         ).year
             else:
-                site_start_year = 1990  
+                site_start_year = 1990
                 # If the start date is unavailable, assume it's early
                 # Unsure why unavailable appears for only one site at
                 # the time of commenting but this gets around it and
@@ -212,14 +216,17 @@ class AURNAPI:
                         "%Y-%m-%d"
                         ).year
             else:
-                site_end_year = dt.datetime.now().year + 1     
+                site_end_year = dt.datetime.now().year + 1
 
-            start_year_in_range = (site_start_year <= start_year <= site_end_year)
+            start_year_in_range = (site_start_year <= start_year <=
+                                   site_end_year)
             end_year_in_range = (site_start_year <= end_year <= site_end_year)
-            data_not_available = (not start_year_in_range and not end_year_in_range)
+            data_not_available = (not start_year_in_range and not
+                                  end_year_in_range)
             # Checks if network started after selected period or ended before
             not_aurn_site = ("AURN" not in str(row["Networks"]))
-            # Checks if site is actually AURN, some sites double up with different
+            # Checks if site is actually AURN, some sites double up with
+            # different
             # names but one of the duplicates doesn't state AURN
             if data_not_available or not_aurn_site:
                 continue
@@ -252,21 +259,21 @@ class AURNAPI:
             if download_code is not None:
                 self.metadata.append(
                         {
-                        "tags": {
-                            "UK-AIR ID": row["UK-AIR ID"],
-                            "EU Site ID": row["EU Site ID"],
-                            "EMEP Site ID": row["EMEP Site ID"],
-                            "Site Name": row["Site Name"],
-                            "Environment Type": row["Environment Type"],
-                            "Zone": row["Zone"],
-                            "Download Code": download_code
+                            "tags": {
+                                "UK-AIR ID": row["UK-AIR ID"],
+                                "EU Site ID": row["EU Site ID"],
+                                "EMEP Site ID": row["EMEP Site ID"],
+                                "Site Name": row["Site Name"],
+                                "Environment Type": row["Environment Type"],
+                                "Zone": row["Zone"],
+                                "Download Code": download_code
                             },
-                        "fields": {
-                            "Latitude": float(row["Latitude"]),
-                            "Longitude": float(row["Longitude"]),
-                            "Northing": float(row["Northing"]),
-                            "Easting": float(row["Easting"]),
-                            "Altitude": float(row["Altitude (m)"])
+                            "fields": {
+                                "Latitude": float(row["Latitude"]),
+                                "Longitude": float(row["Longitude"]),
+                                "Northing": float(row["Northing"]),
+                                "Easting": float(row["Easting"]),
+                                "Altitude": float(row["Altitude (m)"])
                             }
                         }
                     )
@@ -279,14 +286,15 @@ class AURNAPI:
                 f"{download_code}_{year}.csv"
                 )
         try:
-            raw_csv = pd.read_table(csv_url, sep=",", skiprows=4, low_memory=False)
+            raw_csv = pd.read_table(csv_url, sep=",", skiprows=4,
+                                    low_memory=False)
         except urllib.error.HTTPError:
             # If data can't be found, quit out and move on
             self.measurement_csv = pd.DataFrame()
             return None
-        
+
         # Remove brackets from columns
-        # Some csv files have brackets in their pollutant names (e.g 
+        # Some csv files have brackets in their pollutant names (e.g
         # PM<sub>10</sub>) while some don't. Removing the brackets
         # standardises this
         raw_columns = list(raw_csv.columns)
@@ -296,7 +304,7 @@ class AURNAPI:
                 debracketed_columns[column] = remove_brackets(column)
         if len(debracketed_columns.keys()) > 0:
             raw_csv = raw_csv.rename(columns=debracketed_columns)
-        
+
         # Get valid columns
         raw_columns = raw_csv.columns
         pollutants = list()
@@ -304,8 +312,8 @@ class AURNAPI:
         for column in raw_columns:
             if not any(tag in column for tag in ["unit.", "status."]):
                 pollutant_allowed = (
-                    column in allowed_pollutants 
-                    or len(allowed_pollutants) == 0 
+                    column in allowed_pollutants
+                    or len(allowed_pollutants) == 0
                         )
                 if pollutant_allowed:
                     pollutants.append(column)
