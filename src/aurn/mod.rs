220d1913e6376844b79f441b7e268d0a381905e3
CaderIdris <61825162+Joppleganger@users.noreply.github.com>
2022-07-05 18:25:32 +0100

    Check if sites active
    If sites were measuring during requested time period, their metadata is added to a Vec for later use

--- src/aurn/mod.rs
+++ src/aurn/mod.rs
@@ -9,72 +9,71 @@
 /// Downloads a csv containing metadata for all AURN monitoring stations, uses the metadata to
 /// generate URLs pointing to csvs containing hourly averages of yearly measurements 
 
-use serde::Deserialize;
+use std::collections::HashMap;
 
-#[derive(Deserialize, Debug)]
-struct Row {
-    #[serde(rename = "UK-AIR ID")]
-    uk_air_id: String,
-    #[serde(rename = "EU Site ID")]
-    eu_site_id: String,
-    #[serde(rename = "EMEP Site ID")]
-    emep_site_id: Option<String>,
-    #[serde(rename = "Site Name")]
-    site_name: String,
-    #[serde(rename = "Environment Type")]
-    environment_type: String,
-    #[serde(rename = "Zone")]
-    zone: String,
-    #[serde(rename = "Start Date")]
-    start_date: String,
-    #[serde(rename = "End Date")]
-    end_date: Option<String>,
-    #[serde(rename = "Latitude")]
-    latitude: f64,
-    #[serde(rename = "Longitude")]
-    longitude: f64,
-    #[serde(rename = "Northing")]
-    northing: f64,
-    #[serde(rename = "Easting")]
-    easting: f64,
-    #[serde(rename = "Altitude (m)")]
-    altitude: Option<u32>,
-    #[serde(rename = "Networks")]
-    networks: String,
-    #[serde(rename = "AURN Pollutants Measured")]
-    pollutants: Option<String>,
-    #[serde(rename = "Site Description")]
-    site_description: Option<String>,
+use chrono::{DateTime, Utc, MIN_DATETIME};
+use csv::Reader;
+use reqwest::blocking::get;
+use scraper::Html;
+use scraper::Selector;
 
-}
+
+type CSVRow = HashMap<String, String>;
 
 pub struct AURNMetadata {
-    metadata: Vec<Row>,
-    csv_string: String,
-    
-}
-
-impl Default for AURNMetadata {
-    fn default() -> AURNMetadata {
-        let empty_vec: Vec<Row> = Vec::new();
-        AURNMetadata {
-            metadata: empty_vec,
-            csv_string: "".to_string(),
-        }
-    }
-
+    metadata: Vec<CSVRow>,
 }
 
 impl AURNMetadata {
-    pub fn get_metadata(&mut self, query_url: String) {
-        let response = reqwest::blocking::get(&query_url).unwrap().text().unwrap(); 
-        let page = scraper::Html::parse_document(&response);
+
+    pub fn new() -> Self {
+        let empty_vec: Vec<CSVRow> = Vec::new();
+        Self {
+            metadata: empty_vec
+        }
+    }
+
+    pub fn get_metadata(&mut self, query_url: &str, start_date: DateTime<Utc>, end_date: DateTime<Utc>) {
         // Download HTML of metadata page and parse it with scraper
-        let csv_download_link_find = scraper::Selector::parse(r#"a[class="bCSV"]"#).unwrap();
+        let response = get(query_url).unwrap().text().unwrap(); 
+        let page = Html::parse_document(&response);
+        // Find csv download link and download metadata csv
+        let csv_download_link_find = Selector::parse(r#"a[class="bCSV"]"#).unwrap();
         let csv_download_link = page.select(&csv_download_link_find).next().unwrap()
         .value().attr("href").unwrap();
-        self.csv_string = reqwest::blocking::get(csv_download_link).unwrap().text().unwrap();
-        
+        let csv_string = get(csv_download_link).unwrap().text().unwrap();
+
+        // Iterate over csv, deserialize each row into a Row struct and add to metadata Vec
+        let mut csv_reader = Reader::from_reader(csv_string.as_bytes());
+        for result in csv_reader.deserialize() {
+            let record: CSVRow = result.unwrap();
+            let mut site_start_date_str: String = record.get("Start Date").unwrap().to_string() + "T00:00:00Z";
+            let mut site_end_date_str: String = record.get("End Date").unwrap().to_string() + "T00:00:00Z";
+            println!("{}", site_start_date_str);
+            let site_start_date: DateTime<Utc> = match site_start_date_str.len() {
+                // Start date present
+                20 => site_start_date_str.parse::<DateTime<Utc>>().unwrap(),
+                // No start date present
+                _ => MIN_DATETIME,
+            };
+            let site_end_date: DateTime<Utc> = match site_end_date_str.len() {
+                // End date present
+                20 => site_end_date_str.parse::<DateTime<Utc>>().unwrap(),
+                // No end date present
+                _ => Utc::now(),
+            };
+            // Only use metadata if site was measuring during requested range
+            let within_start: bool = start_date < site_end_date;
+            let within_end: bool = end_date > site_start_date;
+            if within_start && within_end {
+                self.metadata.push(record);
+            }
+        }
+    }
+
+    pub fn sites_with_measurement_check(&mut self) {
+
+
     }
 }
 
@@ -86,9 +85,9 @@ mod tests {
     /// Checks whether csv can be downloaded
     #[test]
     fn csv_downloaded() {
-        let mut aurn: AURNMetadata = Default::default();
-        aurn.get_metadata("https://uk-air.defra.gov.uk/networks/find-sites?site_name=&pollutant=9999&group_id=4&closed=true&country_id=9999&region_id=9999&location_type=9999&search=Search+Network&view=advanced&action=results".to_string());
-        assert!(!aurn.csv_string.eq(""));
+        let mut aurn: AURNMetadata = AURNMetadata::new();
+        aurn.get_metadata("https://uk-air.defra.gov.uk/networks/find-sites?site_name=&pollutant=9999&group_id=4&closed=true&country_id=9999&region_id=9999&location_type=9999&search=Search+Network&view=advanced&action=results", "2017-01-01T00:00:00Z".parse::<DateTime<Utc>>().unwrap(), "2020-01-01T00:00:00Z".parse::<DateTime<Utc>>().unwrap());
+        assert!(aurn.metadata.len() > 0);
     }
 
 }
