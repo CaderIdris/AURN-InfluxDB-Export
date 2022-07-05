07156ee6add3844b1393b5d727dcc305dbfbda6f
CaderIdris <61825162+Joppleganger@users.noreply.github.com>
2022-07-05 23:02:08 +0100

    Find site code
    The site codes used to download csvs can now be found. This function and the metadata function should probably be run on init as they are necessary for the rest of the code to function

--- src/aurn/mod.rs
+++ src/aurn/mod.rs
@@ -13,6 +13,7 @@ use std::collections::HashMap;
 
 use chrono::{DateTime, Utc, MIN_DATETIME};
 use csv::Reader;
+use regex::Regex;
 use reqwest::blocking::get;
 use scraper::Html;
 use scraper::Selector;
@@ -49,7 +50,6 @@ impl AURNMetadata {
             let record: CSVRow = result.unwrap();
             let mut site_start_date_str: String = record.get("Start Date").unwrap().to_string() + "T00:00:00Z";
             let mut site_end_date_str: String = record.get("End Date").unwrap().to_string() + "T00:00:00Z";
-            println!("{}", site_start_date_str);
             let site_start_date: DateTime<Utc> = match site_start_date_str.len() {
                 // Start date present
                 20 => site_start_date_str.parse::<DateTime<Utc>>().unwrap(),
@@ -71,9 +71,25 @@ impl AURNMetadata {
         }
     }
 
-    pub fn sites_with_measurement_check(&mut self) {
-
-
+    pub fn get_site_id(&mut self) {
+        let site_regex = Regex::new(r"\?site_id=").unwrap();
+        let id_regex = Regex::new(r"\w*?$").unwrap();
+        for site_index in 0..self.metadata.len() {
+            let site = &self.metadata[site_index];
+            let uk_air_id: &str = site.get("UK-AIR ID").unwrap();
+            let query_url: String = "https://uk-air.defra.gov.uk/networks/site-info?uka_id=".to_string() + uk_air_id;
+            let response = get(query_url).unwrap().text().unwrap();
+            let page = Html::parse_document(&response);
+            let site_code_find = Selector::parse(r#"a[class="bData"]"#).unwrap();
+            let bdata_tags = page.select(&site_code_find).map(|x| x.value().attr("href").unwrap());
+            for bdata_tag in bdata_tags.into_iter() {
+                if site_regex.is_match(bdata_tag) {
+                    let site_code = id_regex.captures(bdata_tag).unwrap().get(0).unwrap().as_str();
+                    self.metadata[site_index].insert("Site Code".to_string(), site_code.to_string());
+                    println!("{:?}", self.metadata[site_index]);
+                }
+            }
+        }
     }
 }
 
@@ -90,5 +106,11 @@ mod tests {
         assert!(aurn.metadata.len() > 0);
     }
 
+    #[test]
+    fn site_id_found() {
+        let mut aurn: AURNMetadata = AURNMetadata::new();
+        aurn.get_metadata("https://uk-air.defra.gov.uk/networks/find-sites?site_name=&pollutant=9999&group_id=4&closed=true&country_id=9999&region_id=9999&location_type=9999&search=Search+Network&view=advanced&action=results", "2017-01-01T00:00:00Z".parse::<DateTime<Utc>>().unwrap(), "2020-01-01T00:00:00Z".parse::<DateTime<Utc>>().unwrap());
+        aurn.get_site_id();
+    }
 }
 
